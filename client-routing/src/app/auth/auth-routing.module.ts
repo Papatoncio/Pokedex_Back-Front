5813fb4b708411967796c88c7518f0818a27148c
Juan Jimenez <juanpij23@hotmail.com>
2022-09-23 12:47:48 -0500

    Front

---
+++ client-routing/src/app/auth/auth-routing.module.ts
@@ -0,0 +1,11 @@
+import { Component, NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+
+export class AppRoutingModule {
+
+}
